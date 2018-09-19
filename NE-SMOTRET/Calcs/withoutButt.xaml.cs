9af54665a09bfc08b7a62a930d43d5b3c53a4bda
Oleg Plaxin <plaxoleg@gmail.com>
2018-09-20 01:44:19 +0300

    first commit

---
+++ NE-SMOTRET/Calcs/withoutButt.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Calcs {
+    public partial  class withoutButt : Window {
+        public withoutButt() {
+            InitializeComponent();
+            value.Focus();
+        }
+
+        private void value_TextChanged(object sender, TextChangedEventArgs e) {
+            answer.Text = Calc(value.Text);
+        }
+
+        private string Calc(String changableValue) {
+            String ans;
+
+            try {
+                var calc = new NCalc.Expression(changableValue).Evaluate();
+                ans = calc.ToString();
+                answer.Foreground = Brushes.Gray;
+
+            }
+            catch {
+                ans = "Не является выражением.";
+                answer.Foreground = Brushes.Gray;
+
+            }
+            return ans;
+        }
+    }
+}
--- NE-SMOTRET/MainWindow.xaml.cs
+++ NE-SMOTRET/MainWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace NE_SMOTRET
 {
@@ -22,6 +10,19 @@ namespace NE_SMOTRET
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new Calcs.withButton().Show();
+        }
+        private void ButtonNo_Click(object sender, RoutedEventArgs e)
+        {
+            new Calcs.withoutButt().Show();
+        }
+        private void Piatnashki_Click(object sender, RoutedEventArgs e)
+        {
+            new Piatnashki.main().Show();
         }
     }
 }
